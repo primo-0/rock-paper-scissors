6d1083645b9c57fd839c273bfabc55b0e7daf1b2
Justin Augustus Macaraeg <justinaugustusm@gmail.com>
2026-07-08 08:10:10 +0800

    fixed button for click event listener

--- 12-rock-paper-scissors.js
+++ 12-rock-paper-scissors.js
@@ -27,17 +27,17 @@
         }
       } 
 
-    document.querySelector('.js-rock-button')
+    document.querySelector('.js-rock-button').
     addEventListener ('click', () => {
       playGame ('rock');
     });
     
-    document.querySelector ('.js-paper-button')
+    document.querySelector ('.js-paper-button').
     addEventListener ('click', () => {
       playGame ('paper');
     });
 
-    document.querySelector ('.js-scissors-button')
+    document.querySelector ('.js-scissors-button').
     addEventListener ('click', () => {
       playGame ('scissors');
     });
